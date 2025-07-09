ede92cb736736bde8ede05057f646f840f907cf1
Aurelio Amerio <aure.amerio@gmail.com>
2025-07-09 16:30:45 +0200

    plotting utils

--- src/gensbi/utils/plotting.py
+++ src/gensbi/utils/plotting.py
@@ -1,5 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from matplotlib.colors import LinearSegmentedColormap
+
+import seaborn as sns
+import pandas as pd
+
+sns.set_style("darkgrid")
 
 
 def plot_trajectories(traj):
@@ -13,4 +20,58 @@ def plot_trajectories(traj):
     ax.set_facecolor('#A6AEBF')
     return fig, ax
 
-#make functions to plot a vector field
+#plot marginals using seaborn's PairGrid
+
+base_color = "#CD5656"  # Base color for the hexbin and kdeplot
+hist_color = "#202A44"  # Color for the histograms 
+
+rgb_base = np.array(mcolors.to_rgb(base_color))
+
+colors = [(rgb_base[0], rgb_base[1], rgb_base[2], 0), # At data value 0, color is rgb_base with alpha 0
+          (rgb_base[0], rgb_base[1], rgb_base[2], 1)] # At data value 1, color is rgb_base with alpha 1
+
+transparent_cmap= LinearSegmentedColormap.from_list("transparent_red", colors, N=256)
+
+def _plot_marginals_2d(data, plot_levels=True, labels=None, gridsize=15, hexbin_kwargs={}, histplot_kwargs={}):
+    if labels is None:
+        labels = [r"$\theta_{}$".format(i) for i in range(1, data.shape[1] + 1)]
+    dataframe = pd.DataFrame(data, columns=labels)
+
+    cmap = hexbin_kwargs.pop('cmap', transparent_cmap)
+    color = hexbin_kwargs.pop('color', [0,0,0,0])  # Default to transparent color
+    gridsize = hexbin_kwargs.pop('gridsize', gridsize)
+
+
+    g = sns.jointplot(data=dataframe, x=labels[0], y=labels[1], kind="hex", height=6, gridsize=gridsize, marginal_kws=dict(bins=gridsize, fill=True, color=hist_color),joint_kws=dict(cmap=cmap, color=color, gridsize=gridsize))
+
+    if plot_levels:
+        levels = np.sort(1-np.array([0.6827, 0.9545]))
+        g.plot_joint(sns.kdeplot, color=hist_color, zorder=3, levels=levels, alpha=1, linewidths=1)
+    return g
+
+def _plot_marginals_nd(data, plot_levels=True, labels=None, gridsize=15, hexbin_kwargs={}, histplot_kwargs={}):
+    if labels is None:
+        labels = [r"$\theta_{}$".format(i) for i in range(1, data.shape[1] + 1)]
+    dataframe = pd.DataFrame(data, columns=labels)
+
+    g = sns.PairGrid(dataframe, corner=True)
+    cmap = hexbin_kwargs.pop('cmap', transparent_cmap)
+    color = hexbin_kwargs.pop('color', [0,0,0,0])  # Default to transparent color
+    gridsize = hexbin_kwargs.pop('gridsize', gridsize)
+
+    g.map_lower(plt.hexbin, gridsize=gridsize, cmap=cmap, color=color, **hexbin_kwargs)
+    if plot_levels:
+        levels = np.sort(1-np.array([0.6827, 0.9545]))
+        g.map_lower(sns.kdeplot, levels=levels, color=hist_color, zorder=3, alpha=1, linewidths=1, **hexbin_kwargs)
+
+    bins = histplot_kwargs.pop('bins', gridsize)
+    fill = histplot_kwargs.pop('fill', True)
+    color = histplot_kwargs.pop('color', hist_color)
+    g.map_diag(sns.histplot, bins=bins, color=color, fill=fill, **histplot_kwargs)
+    return g
+
+def plot_marginals(data, plot_levels=True, labels=None, gridsize=15, hexbin_kwargs={}, histplot_kwargs={}):
+    if data.shape[1] == 2:
+        return _plot_marginals_2d(data, plot_levels=plot_levels, labels=labels, gridsize=gridsize, hexbin_kwargs=hexbin_kwargs, histplot_kwargs=histplot_kwargs)
+    else:
+        return _plot_marginals_nd(data, plot_levels=plot_levels, labels=labels, gridsize=gridsize, hexbin_kwargs=hexbin_kwargs, histplot_kwargs=histplot_kwargs)
