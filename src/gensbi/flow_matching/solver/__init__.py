70373e435c80b056e850461cd17d3695f68a4c2e
Aurelio Amerio <aure.amerio@gmail.com>
2025-07-11 19:38:06 +0200

    up

--- src/gensbi/flow_matching/solver/__init__.py
+++ src/gensbi/flow_matching/solver/__init__.py
@@ -1,9 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the CC-by-NC license found in the
-# LICENSE file in the root directory of this source tree.
-
 from .ode_solver import ODESolver
 from .sde_solver import ZeroEnds, NonSingular
 from .solver import Solver
